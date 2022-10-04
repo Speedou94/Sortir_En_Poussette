be9c4d5c3a4a37aef8c2ac77de8f0747f07fe778
Speedou <j.azran23@gmail.com>
2022-10-04 16:55:55 +0200

    register page

--- app/src/main/java/com/example/sortieenpoussette/MainActivity.java
+++ app/src/main/java/com/example/sortieenpoussette/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.sortieenpoussette;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+        // admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    //password correct
+                    Toast.makeText(MainActivity.this,"Connexion éffectuée avec succès", Toast.LENGTH_LONG).show();
+                }else
+                //password incorrect
+                    Toast.makeText(MainActivity.this,"Connexion impossible", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
+
+class MainRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        EditText username = (EditText) findViewById(R.id.username);
+
+        MaterialButton regbtn = (MaterialButton) findViewById(R.id.registerbtn);
+
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username1 = username.getText().toString();
+                Toast.makeText(MainRegister.this,"Username is"+username1,Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
